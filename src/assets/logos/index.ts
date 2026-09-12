79d60a1d1cbaa916270783882442dbd6d24f10fa
lino167 <zaca793@gmail.com>
2026-09-12 14:07:37 -0300

    feat: add brand logos, favicons, wallpapers, and asset index

---
+++ src/assets/logos/index.ts
@@ -0,0 +1,108 @@
+﻿// Horizontal Logos
+import logoHorizontalDark from './horizontal/logo-horizontal-dark.svg';
+import logoHorizontalLight from './horizontal/logo-horizontal-light.svg';
+import logoHorizontalRetinaDark from './horizontal/logo-horizontal-retina-dark.svg';
+import logoHorizontalRetinaLight from './horizontal/logo-horizontal-retina-light.svg';
+import logoHorizontalPng from './horizontal/logo-horizontal.png';
+
+// Vertical Logos
+import logoVerticalDark from './vertical/logo-vertical-dark.svg';
+import logoVerticalLight from './vertical/logo-vertical-light.svg';
+import logoVerticalRetinaDark from './vertical/logo-vertical-retina-dark.svg';
+import logoVerticalRetinaLight from './vertical/logo-vertical-retina-light.svg';
+
+// Symbol / Icons
+import symbolDark from './symbol/symbol-dark.svg';
+import symbolLight from './symbol/symbol-light.svg';
+import symbolMasterDark from './symbol/symbol-master-dark.svg';
+import symbolMasterLight from './symbol/symbol-master-light.svg';
+
+// Favicons
+import faviconDark from './favicons/favicon-dark.svg';
+import faviconLight from './favicons/favicon-light.svg';
+import faviconPng from './favicons/favicon.png';
+
+// Social
+import githubAvatar from './social/github.png';
+import instagramAvatar from './social/instagram.png';
+import linkedinAvatar from './social/linkedin.png';
+import whatsappAvatar from './social/whatsapp.png';
+
+// Wallpapers
+import wallpaperDesktop from './wallpapers/wallpaper-desktop.png';
+import wallpaperWindows from './wallpapers/windows.png';
+
+export {
+  // Horizontal
+  logoHorizontalDark,
+  logoHorizontalLight,
+  logoHorizontalRetinaDark,
+  logoHorizontalRetinaLight,
+  logoHorizontalPng,
+
+  // Vertical
+  logoVerticalDark,
+  logoVerticalLight,
+  logoVerticalRetinaDark,
+  logoVerticalRetinaLight,
+
+  // Symbol
+  symbolDark,
+  symbolLight,
+  symbolMasterDark,
+  symbolMasterLight,
+
+  // Favicons
+  faviconDark,
+  faviconLight,
+  faviconPng,
+
+  // Social
+  githubAvatar,
+  instagramAvatar,
+  linkedinAvatar,
+  whatsappAvatar,
+
+  // Wallpapers
+  wallpaperDesktop,
+  wallpaperWindows,
+};
+
+export const logos = {
+  horizontal: {
+    dark: logoHorizontalDark,
+    light: logoHorizontalLight,
+    retinaDark: logoHorizontalRetinaDark,
+    retinaLight: logoHorizontalRetinaLight,
+    png: logoHorizontalPng,
+  },
+  vertical: {
+    dark: logoVerticalDark,
+    light: logoVerticalLight,
+    retinaDark: logoVerticalRetinaDark,
+    retinaLight: logoVerticalRetinaLight,
+  },
+  symbol: {
+    dark: symbolDark,
+    light: symbolLight,
+    masterDark: symbolMasterDark,
+    masterLight: symbolMasterLight,
+  },
+  favicons: {
+    dark: faviconDark,
+    light: faviconLight,
+    png: faviconPng,
+  },
+  social: {
+    github: githubAvatar,
+    instagram: instagramAvatar,
+    linkedin: linkedinAvatar,
+    whatsapp: whatsappAvatar,
+  },
+  wallpapers: {
+    desktop: wallpaperDesktop,
+    windows: wallpaperWindows,
+  },
+};
+
+export default logos;
